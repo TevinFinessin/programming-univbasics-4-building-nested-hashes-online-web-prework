a47d0905b4ef6c17918dafa2b585a445f2b096e7
benevolent-demo-1612 <tevinsherman@yahoo.com>
2019-08-01 21:43:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,6 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the 	monopoly = 
-		
-		}monopoly hash you build in the previous method. We're going to add stuff to it here!
+	#copy and past the 	monopoly = monopoly hash you build in the previous method. We're going to add stuff to it here!
 
 end
